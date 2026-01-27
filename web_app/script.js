6ebc0b69e9230a57c40cab92ac4db607734e93bc
Ragulcrazie <ragulcrazieorange@gmail.com>
2026-01-27 23:49:38 +0530

    fix: Restore getWeekNumber function v40

--- web_app/script.js
+++ web_app/script.js
@@ -93,7 +93,14 @@ class GhostEngine {
     }
 }
 
-// ... (getWeekNumber stays same) ...
+// Helper for Week Number
+function getWeekNumber(d) {
+    d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
+    d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay()||7));
+    var yearStart = new Date(Date.UTC(d.getUTCFullYear(),0,1));
+    var weekNo = Math.ceil(( ( (d - yearStart) / 86400000) + 1)/7);
+    return weekNo;
+}
 
 // --- 2. DATA LAYER ---
 
